91382085d2946cc525de461a53af89ffb265e754
Christopher Nugent <awedeven+github@gmail.com>
2018-02-21 00:18:19 -0800

    Nice working version

---
+++ Particle.cpp
@@ -0,0 +1,80 @@
+#include <cmath>
+
+// arbitrary
+const double G = 6.674 * pow(10, -8);
+const int D = 3;
+const double BOUNDS = 300;
+const double REBOUND = 0.75;
+
+class Particle {
+public:
+
+    double pos[D];
+
+    double vel[D];
+
+    double mass;
+
+
+    float color[3];
+
+    Particle() {
+        for (int i = 0; i < 3; ++i)
+        {
+            color[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        }
+
+    };
+
+    // Particle(double m, double p[], double v[]) {
+    //     mass = m;
+    //     for (int i = 0; i < D; ++i)
+    //     {
+    //         pos[i] = p[i];
+    //         vel[i] = p
+    //     }
+    //     // xPos = x, yPos = y; zPos = z;
+    //     // xVel = xv, yVel = yv, zVel = zv;
+
+    // }
+
+    void update(double timeStep) {
+        for (int i = 0; i < D; ++i)
+        {
+            /* code */
+
+            if (pos[i] > BOUNDS && vel[i] > 0) {
+                vel[i] *= -REBOUND;
+                pos[i] = BOUNDS;
+            } else if (pos[i] < -BOUNDS && vel[i] < 0) {
+                vel[i] *= -REBOUND;
+                pos[i] = -BOUNDS;
+            }
+            pos[i] += timeStep * vel[i];
+
+        }
+    }
+
+    void gravitate(Particle& o, double timeStep) {
+        double dis = distance(o);
+        double force = G * mass * o.mass / pow(dis, 2);
+
+
+        double dir[D];
+        for (int i = 0; i < D; ++i)
+        {
+            dir[i] = (pos[i] - o.pos[i]) / dis;
+            vel[i] -= timeStep * force * dir[i] / mass;
+            o.vel[i] += timeStep * force * dir[i] / o.mass;
+        }
+    }
+
+    double distance(const Particle& o) {
+        double sum = 0.000001;
+        for (int i = 0; i < D; ++i)
+        {
+            sum += pow(pos[i] - o.pos[i], 2);
+        }
+        return sqrt(sum);
+    }
+};
---
+++ simulation.cpp
@@ -0,0 +1,154 @@
+#include <GL/glut.h>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include "Particle.cpp"
+
+
+//Sphere
+GLfloat xRotated, yRotated, zRotated;
+GLdouble radius = 1;
+int refreshMills = 5;        // refresh interval in milliseconds [NEW]
+int tickTime = 50;
+int tick = 1;
+// Particle blackHole(20000);
+double totalMass = -1;
+
+
+const int N = 10;
+Particle particles[N];
+
+void display(void);
+void reshape(int x, int y);
+
+
+double randomFloat() {
+	double pos = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	return pos;
+}
+
+void initParticles() {
+	totalMass = 0;
+	for (int i = 0; i < N; i++)
+	{
+		double mass = 100000 * randomFloat();
+		particles[i].mass = mass;
+		totalMass += mass;
+		for (int i = 0; i < D; ++i)
+		{
+			particles[i].pos[i] = 40 * randomFloat() - 20;
+			particles[i].vel[i] = 10 * randomFloat() - 5;
+			/* code */
+		}
+	}
+}
+
+void drawParticles() {
+	double cm[3] = {0, 0, 0};
+	for (int i = 0; i < N; i++)
+	{
+		glColor3f(particles[i].color[0], particles[i].color[1], particles[i].color[2]);
+		glPushMatrix();
+		double pos[3] = {0, 0, 0};
+		for (int j = 0; j < D; ++j)
+		{
+			pos[j] = particles[i].pos[j];
+			cm[j] += (particles[i].mass / totalMass) * pos[j];
+		}
+		glTranslatef(pos[0] / 100, pos[1] / 100, pos[2] / 100);
+		glutSolidSphere(cbrt(particles[i].mass) / 100, 15, 15);
+		glPopMatrix();
+		// std::cout << particles[i].pos[0] << ", "
+		//           << particles[i].pos[1] << ", "
+		//           << particles[i].pos[2] << std::endl;
+	}
+	glColor3f(randomFloat(), randomFloat(), randomFloat());
+	glPushMatrix();
+	glTranslatef(cm[0] / 100, cm[1] / 100, cm[2] / 100);
+	glutSolidSphere(0.1, 15, 15);
+	glPopMatrix();
+}
+
+void update(double timeStep) {
+	for (int i = 0; i < N; i++) {		// include all particles
+		for (int j = i + 1; j < N; j++) {
+			particles[i].gravitate(particles[j], timeStep);
+		}
+		// blackHole.gravitate(particles[i], timeStep);
+		particles[i].update(timeStep);
+	}
+	// particles[0].xPos = 0;
+	// particles[0].yPos = 0;
+	// particles[0].zPos = 0;
+}
+
+// Called back when timer expired [NEW]
+void timer(int value) {
+	glutPostRedisplay();      // Post re-paint request to activate display()
+	glutTimerFunc(refreshMills, timer, 0); // next timer call milliseconds later
+}
+
+int main(int argc, char **argv)
+{
+	srand(time(0));
+	initParticles();
+
+	glutInit(&argc, argv);
+	glutInitWindowSize(550, 550);
+	glutCreateWindow("Sphere");
+
+	glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+	glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
+
+
+	// LIGHTING SHENANIGANS
+	// GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	// GLfloat mat_shininess[] = { 50.0 };
+	// GLfloat light_position[] = { 1, 0, 	0 };
+	// glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	// glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	// glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	// glEnable(GL_LIGHTING);
+	// glEnable(GL_LIGHT0);
+	// END LIGHTING SHENANIGANS
+
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutTimerFunc(0, timer, 0);
+	glutMainLoop();
+	return 0;
+}
+
+
+void display(void)
+{
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glTranslatef(0, 0, -15.5);
+	glRotatef(30, 1, 0, 0);
+	glRotatef(tick / 15.0, 0, 1, 0);
+	glRotatef(tick / 15.0, 1, 0, 0);
+	glRotatef(tick / 15.0, 0, 0, 1);
+	// glRotatef(90, 1, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_DEPTH_BUFFER_BIT);
+	update(tickTime);
+	glColor3f(1, 1, 1);
+	glutWireCube(6);
+	drawParticles();
+	glFlush();
+	tick++;
+}
+
+void reshape(int x, int y)
+{
+	if (y == 0 || x == 0) return;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(39.0, (GLdouble)x / (GLdouble)y, 0.6, 21.0);
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, x, y);  //Use the whole window for rendering
+}
